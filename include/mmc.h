b6e70011124ac652d46986066998150e73ff1e3f
wally.yeh <wally.yeh@atrustcorp.com>
2014-09-29 08:33:32 +0800

    * adding mmc sub-command "bootpart".

--- include/mmc.h
+++ include/mmc.h
@@ -300,6 +300,7 @@ struct mmc {
 	ushort rca;
 	char part_config;
 	char part_num;
+	char boot_part_num;
 	uint tran_speed;
 	uint read_bl_len;
 	uint write_bl_len;
@@ -348,6 +349,8 @@ int mmc_rpmb_read(struct mmc *mmc, void *addr, unsigned short blk,
 		  unsigned short cnt, unsigned char *key);
 int mmc_rpmb_write(struct mmc *mmc, void *addr, unsigned short blk,
 		   unsigned short cnt, unsigned char *key);
+int mmc_switch_boot_part(int dev_num, unsigned int part_num);
+int sd_switch_boot_part(int dev_num, unsigned int part_num);
 /**
  * Start device initialization and return immediately; it does not block on
  * polling OCR (operation condition register) status.  Then you should call
